714f293a504aea92e6a3aeaa58beb2b87c3825f2
Till1983 <till_ermold@web.de>
2026-06-15 12:06:48 +0200

    feat: add significance evaluation imports and define __all__ in evaluation module

---
+++ src/evaluation/__init__.py
@@ -0,0 +1,14 @@
+from src.evaluation.significance import (
+    SignificanceResult,
+    bootstrap_significance_test,
+    mapsswe_test,
+    paired_bootstrap_differences,
+)
+
+__all__ = [
+    "SignificanceResult",
+    "mapsswe_test",
+    "paired_bootstrap_differences",
+    "bootstrap_significance_test",
+]
+
